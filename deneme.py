528038af707b0511b3fb4bc14b8f37463ec6dbea
drfgungor <61504289+drfgungor@users.noreply.github.com>
2023-08-18 04:58:33 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -22,6 +22,7 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        dicti = {"brand": "Ford","model": "Mustang","year": 1964}
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
 
